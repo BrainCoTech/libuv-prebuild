ddcc5d530be6832fc7fa6c2a5501473e8b776c64
zlargon <zlargon1988@gmail.com>
2019-07-16 15:49:19 -0400

    [win] build x86 and x64 static and shared libraries

--- win/build-win.sh
+++ win/build-win.sh
@@ -3,12 +3,31 @@ set -e
 root=$(pwd)
 
 # clean
-rm -rf build *.lib *.dll
+rm -rf build libuv-win*
 
-# cmake build
-cmake -H../libuv -B"build"
-cmake --build "build" --config "Release"
+function build() {
+    cmake --build "build/${ARCH}" --config "Release"
 
-# copy static and shared libraries
-cp build/Release/uv_a.lib  libuv-win.lib
-cp build/Release/uv.dll    libuv-win.dll
+    # static library
+    mv build/${ARCH}/Release/uv_a.lib   libuv-win-${ARCH}.lib
+
+    # shared library (DLL)
+    DLL_DIR="libuv-win-${ARCH}-dll"
+    mv build/${ARCH}/Release/   ${DLL_DIR}
+
+    # pack DLL folder (choco install zip)
+    zip -r ${DLL_DIR}.zip       ${DLL_DIR}
+}
+
+# 1. cmake build x86
+ARCH="x86"
+cmake -H../libuv -B"build/${ARCH}"
+build
+
+# 2. cmake build x64
+ARCH="x64"
+cmake -H../libuv -B"build/${ARCH}" -A"x64"
+build
+
+# remove build folder
+rm -rf build
